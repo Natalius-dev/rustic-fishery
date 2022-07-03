92c69f3964831145b22930a721195994bee51d43
Ant Developers <55980467+Ant-Developers@users.noreply.github.com>
2022-07-03 23:25:10 +0700

    Converted associative arrays to multi-dimensional arrays
    Converted the fish arrays to multi-dimensional arrays to store fish depth and other data in the future

--- main.js
+++ main.js
@@ -7,44 +7,44 @@ tempImg = new Image()
 tempImg.src="pattern_uroko.png"
 var tempImg = []
 
-fish_red = {
-    "flagtail": "https://1.bp.blogspot.com/-56Y9cIeGpE0/X5OcS9dZIhI/AAAAAAABb8A/nN9aD-ZvOp86FZuQa-Zhp7QlpX3cFGQOwCNcBGAsYHQ/s829/fish_ginyugoi.png",
-    "amberjack": "https://1.bp.blogspot.com/-SmXDvlhYF2w/XzXkyv-YZuI/AAAAAAABal4/V34t7_HzQW0Vn9EeVIXG4MtSuo47dm3LgCNcBGAsYHQ/s1600/fish_buri.png",
-    "sweeper": "https://1.bp.blogspot.com/-utG6I90wyjs/XxU0c_Y7TMI/AAAAAAABaLQ/OsTcpkol1XIueZGjKVHuYMRVvsnSgeLKQCNcBGAsYHQ/s1600/fish_hatanpo.png",
-    "mullet": "https://1.bp.blogspot.com/-FEy2YwNkdyI/Xhwp_TbUlhI/AAAAAAABW6Q/n-0c2AZUV2M0OQz9PvIVSw3yFhjNtsNqgCNcBGAsYHQ/s1600/fish_bora.png",
-    "char": "https://1.bp.blogspot.com/-1hQaw1LANXY/XZR9Wcca0KI/AAAAAAABVRs/m3xT7MKZG7ADd7qgvWf8cHDc8mRz1TISACNcBGAsYHQ/s1600/fish_iwana.png"
-}
+fish_red = [
+    ["flagtail", "https://1.bp.blogspot.com/-56Y9cIeGpE0/X5OcS9dZIhI/AAAAAAABb8A/nN9aD-ZvOp86FZuQa-Zhp7QlpX3cFGQOwCNcBGAsYHQ/s829/fish_ginyugoi.png"],
+    ["amberjack", "https://1.bp.blogspot.com/-SmXDvlhYF2w/XzXkyv-YZuI/AAAAAAABal4/V34t7_HzQW0Vn9EeVIXG4MtSuo47dm3LgCNcBGAsYHQ/s1600/fish_buri.png"],
+    ["sweeper", "https://1.bp.blogspot.com/-utG6I90wyjs/XxU0c_Y7TMI/AAAAAAABaLQ/OsTcpkol1XIueZGjKVHuYMRVvsnSgeLKQCNcBGAsYHQ/s1600/fish_hatanpo.png"],
+    ["mullet", "https://1.bp.blogspot.com/-FEy2YwNkdyI/Xhwp_TbUlhI/AAAAAAABW6Q/n-0c2AZUV2M0OQz9PvIVSw3yFhjNtsNqgCNcBGAsYHQ/s1600/fish_bora.png"],
+    ["char", "https://1.bp.blogspot.com/-1hQaw1LANXY/XZR9Wcca0KI/AAAAAAABVRs/m3xT7MKZG7ADd7qgvWf8cHDc8mRz1TISACNcBGAsYHQ/s1600/fish_iwana.png"]
+];
 
-fish_orange = {
-    "black scraper": "https://2.bp.blogspot.com/-ReYngIZ87xA/XNPUsY8dI7I/AAAAAAABS20/Aq9JPLN8Bg4ujGCR8bHqexD3t9VeDePlwCLcBGAs/s800/fish_umadura_hagi.png",
-    "anchovy": "https://1.bp.blogspot.com/-DKTjE-EvcWQ/XVd7FsdBeAI/AAAAAAABUMI/smLyoFcAwpc03d7khqElVWvL5K77u6GCACLcBGAs/s1600/fish_katakuchi_iwashi.png",
-    "salmon": "https://1.bp.blogspot.com/-6tG_gbh_22g/XlyfstkjNJI/AAAAAAABXoM/qImqCHBvk_kNCCUTH61kMiwJxY1cKZcrACNcBGAsYHQ/s1600/fish_benizake.png"
-}
+fish_orange = [
+    ["black scraper", "https://2.bp.blogspot.com/-ReYngIZ87xA/XNPUsY8dI7I/AAAAAAABS20/Aq9JPLN8Bg4ujGCR8bHqexD3t9VeDePlwCLcBGAs/s800/fish_umadura_hagi.png"],
+    ["anchovy", "https://1.bp.blogspot.com/-DKTjE-EvcWQ/XVd7FsdBeAI/AAAAAAABUMI/smLyoFcAwpc03d7khqElVWvL5K77u6GCACLcBGAs/s1600/fish_katakuchi_iwashi.png"],
+    ["salmon", "https://1.bp.blogspot.com/-6tG_gbh_22g/XlyfstkjNJI/AAAAAAABXoM/qImqCHBvk_kNCCUTH61kMiwJxY1cKZcrACNcBGAsYHQ/s1600/fish_benizake.png"]
+]
 
-fish_yellow = {
-    "sea roach": "https://1.bp.blogspot.com/-camzFggE1J4/WZP3Um4j-qI/AAAAAAABF9U/39LAYH0Wwic5BGL88ferIWBPqQfgIuR6ACLcBGAs/s800/bug_funamushi.png"
-}
+fish_yellow = [
+    ["sea roach", "https://1.bp.blogspot.com/-camzFggE1J4/WZP3Um4j-qI/AAAAAAABF9U/39LAYH0Wwic5BGL88ferIWBPqQfgIuR6ACLcBGAs/s800/bug_funamushi.png"]
+]
 
-fish_green = {
-    "message in a bottle": "https://3.bp.blogspot.com/-i1lokFIPyKw/WASJE3lw_II/AAAAAAAA-8w/Y4z5JBad75cUSXGaTGbIEGIlHgElHagpQCLcB/s800/bin_tegami.png",
-    "pudgy cusk-eel": "https://2.bp.blogspot.com/-Pr-yp-ANK3E/W9592dVfVmI/AAAAAAABP5U/kp4n8woJiGQ9vqrCumwBHxHwdCuPmNhmwCLcBGAs/s800/shinkai_fish_sokobouzu.png"
-}
+fish_green = [
+    ["message in a bottle", "https://3.bp.blogspot.com/-i1lokFIPyKw/WASJE3lw_II/AAAAAAAA-8w/Y4z5JBad75cUSXGaTGbIEGIlHgElHagpQCLcB/s800/bin_tegami.png"],
+    ["pudgy cusk-eel", "https://2.bp.blogspot.com/-Pr-yp-ANK3E/W9592dVfVmI/AAAAAAABP5U/kp4n8woJiGQ9vqrCumwBHxHwdCuPmNhmwCLcBGAs/s800/shinkai_fish_sokobouzu.png"]
+]
 
-for(var x=0;x<Object.values(fish_red).length;x++) {
+for(var x=0;x<fish_red.length;x++) {
     tempImg[x] = new Image()
-    tempImg[x].src = Object.values(fish_red)[x]
+    tempImg[x].src = fish_red[x][1]
 }
-for(var x=0;x<Object.values(fish_orange).length;x++) {
+for(var x=0;x<fish_orange.length;x++) {
     tempImg[x] = new Image()
-    tempImg[x].src = Object.values(fish_orange)[x]
+    tempImg[x].src = fish_orange[x][1]
 }
-for(var x=0;x<Object.values(fish_yellow).length;x++) {
+for(var x=0;x<fish_yellow.length;x++) {
     tempImg[x] = new Image()
-    tempImg[x].src = Object.values(fish_yellow)[x]
+    tempImg[x].src = fish_yellow[x][1]
 }
-for(var x=0;x<Object.values(fish_green).length;x++) {
+for(var x=0;x<fish_green.length;x++) {
     tempImg[x] = new Image()
-    tempImg[x].src = Object.values(fish_green)[x]
+    tempImg[x].src = fish_green[x][1]
 }
 
 const fish_dom = document.getElementById("fish_dom");
@@ -63,20 +63,20 @@ function getFish(fish_color)
 {
     var size = randomInterval(35,52.5);
     var rot = randomInterval(-30,30);
-    let fish = parseInt(randomInterval(0,Object.keys(fish_color).length-1));
+    let fish = parseInt(randomInterval(0,fish_color.length-1));
     
-    fish_dom.setAttribute("src", Object.values(fish_color)[fish]);
+    fish_dom.setAttribute("src", fish_color[fish][1]);
     fish_dom.style.zIndex = "0";
     fish_dom.style.transform = "scale("+size+"%) rotate("+rot+"deg)";
     
-    if(vowel.test((Object.keys(fish_color)[fish][0])))
+    if(vowel.test(fish_color[fish][0][0]))
     {
-        fish_text.innerHTML = "You found an " + Object.keys(fish_color)[fish] + "!";
-        fish_dom.setAttribute("alt", "Illustration of an " + Object.keys(fish_color)[fish]);
+        fish_text.innerHTML = "You found an " + fish_color[fish][0] + "!";
+        fish_dom.setAttribute("alt", "Illustration of an " + fish_color[fish][0]);
     }
     else {
-        fish_text.innerHTML = "You found a " + Object.keys(fish_color)[fish] + "!";
-        fish_dom.setAttribute("alt", "Illustration of a " + Object.keys(fish_color)[fish]);
+        fish_text.innerHTML = "You found a " + fish_color[fish][0] + "!";
+        fish_dom.setAttribute("alt", "Illustration of a " + fish_color[fish][0]);
     }
 }
 
